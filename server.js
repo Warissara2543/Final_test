c88f7034f08085ce5e6be0a16d18cdb104efd4e3
Jirachot <s6030211005@phuket.psu.ac.th>
2019-12-03 17:37:21 +0700

    #4 commit

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // #3 Serve static content in folder frontend
+mongoose.connect('mongodb://localhost:27017/coc', { useUnifiedTopology: true, useNewUrlParser: true }); // connect to our database
 var Product = require('./models/product');
 // ===============================
 
@@ -34,7 +35,49 @@ router.get('/products', products.getAllProducts);
 router.get('/products/:pid', products.getProductById);
 
 // #4 Complete the routing for POST, PUT, DELETE
+//Product APIs
+app.post('/api/product', function (req, res) {
+    //Insert data to mongodb
+    var newproduct = req.body;
+    var product = new Product(newproduct);
+    product.save(function (err) {
+        if (err) res.status(500).json(err);
+        res.json({ status: "Added a product" });
+    });
+});
 
+app.get('/api/product', function (req, res) {     //showdetail
+    Product.find(function (err, product) {
+        if (err) res.status(500).json(err);
+        res.json(product);
+    });
+});
+
+app.get('/api/product/:id', function (req, res) {  //sert
+    var id = req.params.id;
+    Product.find({"_id":id}, function (err, product) {
+        if (err) res.status(500).json(err);
+        res.json(product);
+    });
+});
+
+app.put('/api/product/:id', function (req, res) {  //update
+    var id = req.params.id;
+    var updateproduct = req.body;
+    Product.findByIdAndUpdate(id, updateproduct, function(err){
+        if (err) res.status(500).json(err);
+        res.json({status: "Update a product"});
+    })
+});
+
+app.delete('/api/product/:id', function (req, res) {  //update
+    var id = req.params.id;
+    var updateproduct = req.body;
+    Product.findByIdAndRemove(id, updateproduct, function(err){
+        if (err) res.status(500).json(err);
+        res.json({status: "Delete a product"});
+    })
+});
 // ===============================
 
 
